5e1d9e4431ab745b6ff9ee13066f9a7073f6da8d
shengyugithub <shengyuemail@126.com>
2019-07-29 16:30:04 +0800

    first change

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from blog.extensions import db
-
+from datetime import datetime
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,20 +7,31 @@ class Post(db.Model):
     author = db.Column(db.String(20), nullable=False)
     content = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime)
-    category
-    comments
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship("Category", back_populates="posts")
+    comment = db.relationship('Comment', back_populates='post')
 
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.string(20), nullable=False)
-    post
+    posts = db.relationship('Post', back_populates='category')
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    reply = db.Column(db.String(100))  #如果超出？
-    author
-    comment 
+    reply = db.Column(db.Text)  #如果超出？
+    author = db.Column(db.String(30))
+    time = db.Column(db.DateTime,default=datetime.utcnow)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    replied_id = db.Column(db.Integer, db.ForeignKey('comment.id'))
+    post = db.relationship('Post', back_populates='comment')
+    replies = db.relationship('Comment', back_populates='replied', cascade='all')
+    replied = db.relationship('Comment', back_populates='replies')
+
+class Links(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20))
+    link = db.Column(db.String(100), nullable=False)
 
 
 
